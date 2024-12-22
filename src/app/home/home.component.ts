23088aa81d88c0802a465b42f75d5e449108b325
weikhang95 <CWK95@hotmail.com>
2024-12-22 17:57:57 +0800

    Update to ng-19

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,235 +11,212 @@ const baseStyles =  style({
 })
 
 @Component({
-  selector: 'photo-home',
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
-  animations: [
-    trigger('routeAnim', [
-      transition(':increment', [
-        style({
-          position: 'relative',
-          overflow: 'hidden'
-        }),
-
-        query(':enter, :leave', [
-          baseStyles
-        ], { optional: true }),
-
-
-        group([
-          query(':leave', [
-            animate('200ms ease-in', style({
-              opacity: 0,
-              transform: 'translateX(-50px)'
-            }))
-          ], { optional: true }),
-          query(':enter', [
-            style({
-              transform: 'translateX(50px)',
-              opacity: 0,
-            }),
-            animate('250ms 120ms ease-out', style({
-              opacity: 1,
-              transform: 'translateX(0)'
-            }))
-          ], { optional: true })
+    selector: 'photo-home',
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
+    animations: [
+        trigger('routeAnim', [
+            transition(':increment', [
+                style({
+                    position: 'relative',
+                    overflow: 'hidden'
+                }),
+                query(':enter, :leave', [
+                    baseStyles
+                ], { optional: true }),
+                group([
+                    query(':leave', [
+                        animate('200ms ease-in', style({
+                            opacity: 0,
+                            transform: 'translateX(-50px)'
+                        }))
+                    ], { optional: true }),
+                    query(':enter', [
+                        style({
+                            transform: 'translateX(50px)',
+                            opacity: 0,
+                        }),
+                        animate('250ms 120ms ease-out', style({
+                            opacity: 1,
+                            transform: 'translateX(0)'
+                        }))
+                    ], { optional: true })
+                ])
+            ]),
+            transition(':decrement', [
+                style({
+                    position: 'relative',
+                    overflow: 'hidden'
+                }),
+                query(':enter, :leave', [
+                    baseStyles
+                ], { optional: true }),
+                group([
+                    query(':leave', [
+                        animate('200ms ease-in', style({
+                            opacity: 0,
+                            transform: 'translateX(50px)'
+                        }))
+                    ], { optional: true }),
+                    query(':enter', [
+                        style({
+                            transform: 'translateX(-50px)',
+                            opacity: 0,
+                        }),
+                        animate('250ms 120ms ease-out', style({
+                            opacity: 1,
+                            transform: 'translateX(0)'
+                        }))
+                    ], { optional: true })
+                ])
+            ]),
+            transition('* => secondary', [
+                style({
+                    position: 'relative',
+                    //overflow: 'hidden'
+                }),
+                query(':enter, :leave', [
+                    style({
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        height: '100%'
+                    })
+                ], { optional: true }),
+                group([
+                    query(':leave', [
+                        animate('200ms ease-in', style({
+                            opacity: 0,
+                            transform: 'scale(0.8)'
+                        }))
+                    ], { optional: true }),
+                    query(':enter', [
+                        style({
+                            transform: 'scale(1.2)',
+                            opacity: 0,
+                        }),
+                        animate('250ms 120ms ease-out', style({
+                            opacity: 1,
+                            transform: 'scale(1)'
+                        }))
+                    ], { optional: true })
+                ])
+            ]),
+            transition(':decrement', [
+                style({
+                    position: 'relative',
+                    overflow: 'hidden'
+                }),
+                query(':enter, :leave', [
+                    baseStyles
+                ], { optional: true }),
+                group([
+                    query(':leave', [
+                        animate('200ms ease-in', style({
+                            opacity: 0,
+                            transform: 'translateX(50px)'
+                        }))
+                    ], { optional: true }),
+                    query(':enter', [
+                        style({
+                            transform: 'translateX(-50px)',
+                            opacity: 0,
+                        }),
+                        animate('250ms 120ms ease-out', style({
+                            opacity: 1,
+                            transform: 'translateX(0)'
+                        }))
+                    ], { optional: true })
+                ])
+            ]),
+            transition('secondary => *', [
+                style({
+                    position: 'relative',
+                    //overflow: 'hidden'
+                }),
+                query(':enter, :leave', [
+                    style({
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        height: '100%'
+                    })
+                ], { optional: true }),
+                group([
+                    query(':leave', [
+                        animate('200ms ease-in', style({
+                            opacity: 0,
+                            transform: 'scale(1.25)'
+                        }))
+                    ], { optional: true }),
+                    query(':enter', [
+                        style({
+                            transform: 'scale(0.8)',
+                            opacity: 0,
+                        }),
+                        animate('250ms 120ms ease-out', style({
+                            opacity: 1,
+                            transform: 'scale(1)'
+                        }))
+                    ], { optional: true })
+                ])
+            ]),
+            transition(':decrement', [
+                style({
+                    position: 'relative',
+                    overflow: 'hidden'
+                }),
+                query(':enter, :leave', [
+                    style({
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        height: '100%'
+                    })
+                ], { optional: true }),
+                group([
+                    query(':leave', [
+                        animate('200ms ease-in', style({
+                            opacity: 0,
+                            transform: 'translateX(50px)'
+                        }))
+                    ], { optional: true }),
+                    query(':enter', [
+                        style({
+                            transform: 'translateX(-50px)',
+                            opacity: 0,
+                        }),
+                        animate('250ms 120ms ease-out', style({
+                            opacity: 1,
+                            transform: 'translateX(0)'
+                        }))
+                    ], { optional: true })
+                ])
+            ])
+        ]),
+        trigger('bgAnim', [
+            transition(':leave', [
+                animate(1000, style({
+                    opacity: 0
+                }))
+            ])
+        ]),
+        trigger('fadeAnim', [
+            transition(':enter', [
+                style({ opacity: 0 }),
+                animate(250, style({
+                    opacity: 1
+                }))
+            ]),
+            transition(':leave', [
+                animate(250, style({ opacity: 0 }))
+            ])
         ])
-      ]),
-
-      transition(':decrement', [
-        style({
-          position: 'relative',
-          overflow: 'hidden'
-        }),
-
-        query(':enter, :leave', [
-          baseStyles
-        ], { optional: true }),
-
-        group([
-          query(':leave', [
-            animate('200ms ease-in', style({
-              opacity: 0,
-              transform: 'translateX(50px)'
-            }))
-          ], { optional: true }),
-          query(':enter', [
-            style({
-              transform: 'translateX(-50px)',
-              opacity: 0,
-            }),
-            animate('250ms 120ms ease-out', style({
-              opacity: 1,
-              transform: 'translateX(0)'
-            }))
-          ], { optional: true })
-        ])
-      ]),
-
-      transition('* => secondary',[
-        style({
-          position: 'relative',
-          //overflow: 'hidden'
-        }),
-
-        query(':enter, :leave', [
-          style({
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%'
-          })
-        ], { optional: true }),
-
-
-        group([
-          query(':leave', [
-            animate('200ms ease-in', style({
-              opacity: 0,
-              transform: 'scale(0.8)'
-            }))
-          ], { optional: true }),
-          query(':enter', [
-            style({
-              transform: 'scale(1.2)',
-              opacity: 0,
-            }),
-            animate('250ms 120ms ease-out', style({
-              opacity: 1,
-              transform: 'scale(1)'
-            }))
-          ], { optional: true })
-        ])
-      ]),
-
-      transition(':decrement', [
-        style({
-          position: 'relative',
-          overflow: 'hidden'
-        }),
-
-        query(':enter, :leave', [
-          baseStyles
-        ], { optional: true }),
-
-        group([
-          query(':leave', [
-            animate('200ms ease-in', style({
-              opacity: 0,
-              transform: 'translateX(50px)'
-            }))
-          ], { optional: true }),
-          query(':enter', [
-            style({
-              transform: 'translateX(-50px)',
-              opacity: 0,
-            }),
-            animate('250ms 120ms ease-out', style({
-              opacity: 1,
-              transform: 'translateX(0)'
-            }))
-          ], { optional: true })
-        ])
-      ]),
-
-      transition('secondary => *',[
-        style({
-          position: 'relative',
-          //overflow: 'hidden'
-        }),
-
-        query(':enter, :leave', [
-          style({
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%'
-          })
-        ], { optional: true }),
-
-
-        group([
-          query(':leave', [
-            animate('200ms ease-in', style({
-              opacity: 0,
-              transform: 'scale(1.25)'
-            }))
-          ], { optional: true }),
-          query(':enter', [
-            style({
-              transform: 'scale(0.8)',
-              opacity: 0,
-            }),
-            animate('250ms 120ms ease-out', style({
-              opacity: 1,
-              transform: 'scale(1)'
-            }))
-          ], { optional: true })
-        ])
-      ]),
-
-      transition(':decrement', [
-        style({
-          position: 'relative',
-          overflow: 'hidden'
-        }),
-
-        query(':enter, :leave', [
-          style({
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%'
-          })
-        ], { optional: true }),
-
-        group([
-          query(':leave', [
-            animate('200ms ease-in', style({
-              opacity: 0,
-              transform: 'translateX(50px)'
-            }))
-          ], { optional: true }),
-          query(':enter', [
-            style({
-              transform: 'translateX(-50px)',
-              opacity: 0,
-            }),
-            animate('250ms 120ms ease-out', style({
-              opacity: 1,
-              transform: 'translateX(0)'
-            }))
-          ], { optional: true })
-        ])
-      ])
-
-
-    ]),
-
-    trigger('bgAnim',[
-      transition(':leave', [
-        animate(1000, style({
-          opacity:0
-        }))
-      ])
-    ]),
-
-    trigger('fadeAnim', [
-      transition(':enter', [
-        style({ opacity:0}),
-        animate(250, style({
-          opacity:1
-        }))
-      ]),
-      transition(':leave', [
-        animate(250, style({opacity:0}))
-      ])
-    ])
-  ]
+    ],
+    standalone: false
 })
 export class HomeComponent {
 
